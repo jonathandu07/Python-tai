ab3ad034ea929fc085abe50cc4b34d31afaac5e0
jonathan YHWH <93312510+jonathandu07@users.noreply.github.com>
2023-12-14 10:05:19 +0100

    exercice python TAI

---
+++ TAI-Variable.py
@@ -0,0 +1,67 @@
+# déclaration variable
+livre = "Gatsby le Magnifique"
+
+# modification variable
+livre = "Harry Potter"
+
+# Création  d'un personnage
+nom = "Doe"
+prenom = "John"
+age = 25
+message = f"Bonjour, je m'appelle {prenom} {nom} et j'ai {age} ans."
+print(message)
+
+# Les opérations logiques
+
+# addition
+resultat_addition = 1 + 2
+
+# Les soustractions
+resultat_soustractions = 10 - 5
+
+# Les multiplications
+resultat_multiplication = 3 * 4
+
+# Les divisions
+resultat_division = 20 / 5
+
+# Les modulos ( Reste de la division )
+reste_division = 10 % 3
+
+# Les exposants ( pour avoir la puissance )
+exposant = 2 ** 4
+
+# Préferer mettre des espaces pour plus de lisibilité
+resultat = 10 * (5 + 3) / 2
+
+# Utilisation des nombres décimaux
+resultat_decimal = 10.0 / 3
+
+# Les types de données
+
+# les entiers (Integers)
+age = 25
+nombre_d_articles = 15
+
+# Les nombres flotants / à virgules (Floats)
+prix = 19.99
+ratio = 3.14
+
+# Chaînes de caractères (Strings)
+nom = "Alice"
+phrase = "Bonjour, comment ça va ?"
+
+# Les booléens
+# Les booléens ont seulement deux valeurs : True (Vrai) ou False (Faux).
+
+est_vrai = True
+est_faux = False
+
+# Vérifier le type d'une variable
+type_entier = type(42) #<class 'int'>
+type_virgule_flottante = type(3.14) #<class "float">
+type_chaine = type("Python") #<class 'str'>
+type_booleen = type(True) #<class 'bool'>
+
+
+
---
+++ exo_variables.py
@@ -0,0 +1,12 @@
+# Exercice 1:
+
+# Demande à l'utilisateur de saisir son nom
+nom = input("Quel est votre nom\n")
+
+# Demande de l'age de l'utilisateur
+age = int(input("Quel age avez-vous ?\n"))
+
+# afficher le message
+print("Bonjour, {} : Vous avez {} ans.".format(nom, age))
+
+# Exercice 2:
