0a7e5bca8fc798cb071e02602ee33a88ef955770
jonathan YHWH <93312510+jonathandu07@users.noreply.github.com>
2023-12-14 10:58:04 +0100

    autre solution exo 3

--- exo_variables.py
+++ exo_variables.py
@@ -56,3 +56,43 @@ nombre = int(input("Saisissez un nombre : "))
 
 # afficher le nombre en lettres
 print(dict_nombres[nombre])
+
+# Autres solution pour l'exercice 3
+
+# Dictionnaire contenant les noms en dizaine
+dict_dizaine = {
+    1:"dix",
+    2:"vingt",
+    3:"trente",
+    4:"quarante",
+    5:"cinquante",
+    6:"soixante",
+    7:"soixante-dix",
+    8:"quatre-vingt",
+    9:"quatre-vingt-dix"
+}
+
+# Dictionnaire contenant les noms des unités
+dict_unite = {
+    1:"un",
+    2:"deux",
+    3:"trois",
+    4:"quatre",
+    5:"cinq",
+    6:"six",
+    7:"sept",
+    8:"huit",
+    9:"neuf"
+}
+
+# Demande à l'utilisateur de saisir un nombre
+nombre = int(input("Saisissez un nombre : \n"))
+
+# calcule la dizaine !
+# // = division entière qui signifie : 70 // 10 = 7
+dizaine = nombre // 10
+
+# Calcule l'unité
+unite = nombre % 10
+
+print(dict_dizaine[dizaine] + " " + dict_unite[unite])
