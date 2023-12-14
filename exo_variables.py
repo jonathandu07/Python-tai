70fe5ae318fde100154f184d04b646014f8c5b5a
jonathan YHWH <93312510+jonathandu07@users.noreply.github.com>
2023-12-14 11:51:15 +0100

    exercice 5

--- exo_variables.py
+++ exo_variables.py
@@ -95,4 +95,63 @@ dizaine = nombre // 10
 # Calcule l'unité
 unite = nombre % 10
 
-print(dict_dizaine[dizaine] + " " + dict_unite[unite])
+# affiche le nombre en lettres
+print(dict_dizaine[dizaine] + "-" + dict_unite[unite])
+
+# Exercice 4:
+
+# Demande à l'utilisateur de saisir un nombre
+nombre = int(input("Saisissez un nombre : "))
+
+# Inversion du nombre
+nombre_inverse = 0 #Initialisation du nombre inversé à zero
+while nombre > 0: #Tant que le nombre original est supérieur à zéro
+    nombre_inverse = nombre_inverse * 10 + nombre % 10 #Inversion du nombre en ajoutant le dernier chiffre
+    nombre //= 10 # Suppression du dernier chiffre du nombre original
+    
+# afficher le nombre inversé
+print(nombre_inverse)
+
+# Autre solution (beaucoup plus simple)
+
+# On demande le nombre à l'utilisateur
+nombre = int(input("Saissez le nombre : "))
+
+# Inversion du nombre
+nombre_inverse = 0 #Initialisation du nombre inversé à zero
+
+for chiffre in reversed(str(nombre)): # Pour chaque chiffre dans le nombre, en partant de la fin
+    nombre_inverse = nombre_inverse * 10 + int(chiffre) # Inversion du nombre en ajoutant chaque chiffre
+    
+# Afficher le nombre inverse
+print(nombre_inverse)
+
+# Exercice 5:
+
+# Demande à l'utilisateur de saisir un nombre
+nombre = int(input("Saissez le nombre : "))
+
+# Determine le type du nombre !
+if nombre > 0: # Si le nombre est positif
+    type_nombre = "Positif"
+elif nombre < 0: # Si le nombre est négatif
+    type_nombre = "Négatif"
+else: # Si le nombre est nul
+    type_nombre = "Nul"
+
+# Affiche le type du nombre
+print("Le nombre est {}".format(type_nombre))
+
+# autre solution plus simple 
+
+# Demande à l'utilisateur de saisir un nombre
+nombre = int(input("Saissez le nombre : "))
+
+if abs(nombre) > 0: #Si la valeur absolue du nombre est supérieur à 0
+    type_nombre = "Positif" if nombre > 0 else "Négatif" # Utilisation d'une expression conditionnelle pour déterminer le type
+else:
+    type_nombre = "Nul"
+    
+
+# Affiche le type du nombre
+print("Le nombre est {}".format(type_nombre))
